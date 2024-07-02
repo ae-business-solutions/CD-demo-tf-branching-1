6261b05724f0348a2308b7c589cc46d8eae56f22
Alex Holt <alex.holt@aebs.com>
2024-07-02 12:50:47 -0500

    Adding storage account to terraform

--- main.tf
+++ main.tf
@@ -2,3 +2,15 @@ resource "azurerm_resource_group" "terraform_demo" {
   name     = "cd-rg-demo-tf-branching-1"
   location = "Central US"
 }
+
+resource "random_id" "storage" {
+  byte_length = 8
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "tfexample${ random_id.storage.id }"
+  resource_group_name      = azurerm_resource_group.terraform_demo.name
+  location                 = azurerm_resource_group.terraform_demo.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
