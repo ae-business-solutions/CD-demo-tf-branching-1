d373f1d8553a1eed0780430dddf357a83c2433ee
alexholt1 <alex.holt@aebs.com>
2024-07-09 10:19:22 -0500

    update state managment

--- providers.tf
+++ providers.tf
@@ -11,7 +11,7 @@ terraform {
     resource_group_name  = "cd-rg-shared-demo"      # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
     storage_account_name = "cddemostorage"          # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstate-branching"      # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "prod.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    #key                  = "<env>.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
 
