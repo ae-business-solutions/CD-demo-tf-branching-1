9ef749981777087f32927ded571301ac9c2bd3d6
alexholt1 <alex.holt@aebs.com>
2024-07-09 10:25:22 -0500

    azure locks container not file

--- providers.tf
+++ providers.tf
@@ -10,7 +10,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "cd-rg-shared-demo"      # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
     storage_account_name = "cddemostorage"          # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "tfstate-branching"      # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    #container_name       = "tfstate-branching"      # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     #key                  = "<env>.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
