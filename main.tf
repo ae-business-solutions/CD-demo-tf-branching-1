2788ffeb09ae737e8bea3f0d94f0b27a5b06ecd7
Alex Holt <alex.holt@aebs.com>
2024-10-29 14:41:43 -0500

    I dont need the storage account

--- main.tf
+++ main.tf
@@ -17,11 +17,11 @@ resource "random_id" "storage" {
   byte_length = 4
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "tfexample${random_id.storage.hex}"
-  resource_group_name      = azurerm_resource_group.terraform_demo.name
-  location                 = azurerm_resource_group.terraform_demo.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  tags                     = local.tags
-}
+# resource "azurerm_storage_account" "example" {
+#   name                     = "tfexample${random_id.storage.hex}"
+#   resource_group_name      = azurerm_resource_group.terraform_demo.name
+#   location                 = azurerm_resource_group.terraform_demo.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+#   tags                     = local.tags
+# }
